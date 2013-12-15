44b95f1b3404eaa8408e1c1a9ed94916fb7ac216
Charles DuBose <kayakyakr@gmail.com>
2013-12-15 01:02:00 -0600

    Successful deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,36 +5,25 @@ set :repo_url, 'git://github.com/Yakrware/sum_times.git'
 
 set :deploy_to, "/home/ubuntu/#{fetch(:application)}"
 
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
-
 set :linked_files, %w{config/application.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
 
 set :rvm1_ruby_version, '2.0.0-p353'
-#before 'deploy', 'rvm1:install:rvm'  # install/update RVM
-#before 'deploy', 'rvm1:install:ruby'  # install/update Ruby
+
+set :unicorn_pid, -> { File.join(shared_path, "tmp", "pids", "unicorn.pid") }
 
 namespace :deploy do  
   desc 'Restart application'
   task :restart do
     invoke 'unicorn:restart'
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
     end
   end
   
@@ -43,7 +32,6 @@ namespace :deploy do
   end
   
   after :finishing, 'deploy:cleanup'
-
 end
 
 remote_file shared_path.join('config/application.yml') => 'config/application.yml', roles: :app
