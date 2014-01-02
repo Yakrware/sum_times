778770d7c941d81c2bc6647414dd4ec11aa49323
Charles DuBose <kayakyakr@gmail.com>
2014-01-01 22:19:11 -0600

    not returning messages when a user uses the wrong email or password

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,7 @@ module ApplicationHelper
     content << %Q{<div class="alert alert-danger">#{flash[:error]}</div>} unless flash[:error].blank?
     content << %Q{<div class="alert alert-info">#{flash[:notice]}</div>} unless flash[:notice].blank?
     content << %Q{<div class="alert alert-warning">#{flash[:warning]}</div>} unless flash[:warning].blank?
+    content << %Q{<div class="alert alert-warning">#{flash[:alert]}</div>} unless flash[:alert].blank?
     content << %Q{<div class="alert alert-success">#{flash[:success]}</div>} unless flash[:success].blank?
     unless flash[:errors].blank?
       content << "<div class=\"alert alert-danger\"><ul class=\"unstyled\">"
